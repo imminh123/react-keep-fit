ebc907dca5616f6851ccbe3c3ecdfe8798f3d28a
CAJonathan <dns140198@gmail.com>
2018-12-09 05:12:15 +0700

    aa

--- react-front-end/src/config.js
+++ react-front-end/src/config.js
@@ -1,9 +1,3 @@
 export default {
-<<<<<<< HEAD
-	rootPath:
-	  process.env.REACT_APP_STATE === "localhost" ? "http://localhost:6969" : ""
-  };
-=======
     rootPath: "http://localhost:8888"
 };
->>>>>>> 8a781fbf71d1102cfe4e7678a357a3ba71326695
